9f6e4c27b5db310225c105681811820fb642b18a
David Almeida <contato.davidalmeida@outlook.com>
2023-01-02 23:17:35 -0300

    ✅ Add: 404 page component

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "../styles/404.module.css";
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h3>404 | Esta página não existe.</h3>
+      <figure>
+        <Image src="/images/ash.png" width="100" height="200" alt="Ash" />
+      </figure>
+    </div>
+  )
+}
