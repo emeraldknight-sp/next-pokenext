e9505c4eb4a81cf926149b5d32e78f788ac11e0f
David Almeida <contato.davidalmeida@outlook.com>
2023-01-04 13:14:27 -0300

    ✅ Update: About page component

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className={styles.about}>
       <h2>Sobre o projeto</h2>
-      <p>Este projeto tem como finalidade a prática e o aprendizado do framework Next (baseado e construído sobre a biblioteca React.JS), a estilização com CSS Modules e o consumo de APIs para o desenvolvimento deste projeto.</p>
+      <p>Este projeto foi desenvolvido com base nas aulas de Matheus Battisti no Youtube para o aprendizado do framework Next (baseado em React), para a prática da estilização utilizando CSS Modules, e além disso, também para o consumo de APIs e a disposição dos dados para o usuário construindo uma interface que seja responsiva para diferentes tamanhos de viewport de cada usuário com Mobile First.</p>
       <Image src="/images/logo.png" width="200" height="100" alt="Charizard" />
     </div>
   )
