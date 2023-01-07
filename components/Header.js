aabd3031d52004549eaf382192a5d87a513a4748
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 00:08:44 -0300

    ✅ Update: Header component

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import Container from "./Container";
 
 import styles from "../styles/Header.module.css";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <header className={styles.header}>
       <Container>
-        <Link href="/" className={styles.logo}>
-          <figure>
-            <Image
-              src="/images/ball.png"
-              width="30"
-              height="30"
-              alt="PokeNext"
-              priority
-            />
-          </figure>
-          <h1>Pokenext</h1>
-        </Link>
+        <Logo width="30" height="30" />
         <nav className={styles.menu}>
           <ul>
             <li>
